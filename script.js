c6cf1999c8ecd4cfd862bb57a68253fa3e86e033
abkarim <akskarim17@gmail.com>
2022-04-27 18:55:45 +0600

    Updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /**
  * Generate animation time
  * 
- * @param Number text width in px
- * @param Number speed
+ * @param {Number} text width in px
+ * @param {Number} speed
  * 
- * @return Number animation seconds
+ * @return {Number} animation seconds
  */
 function generateAnimationSeconds(content_width, speed) {
 
@@ -19,8 +19,98 @@ function generateAnimationSeconds(content_width, speed) {
 
 }
 
+/**
+ * Set property on document
+ * 
+ * @param {String} property name
+ * @param {*} property value
+ */
+function setPropertyOnDocument(property_name, property_value) {
+    document.documentElement.style.setProperty( property_name, property_value);
+}
+
+// Animation property
+const tickerSpeed = 0.5;
+const tickerDirection = 'left';
+const tickerDelay = 0;
+const tickerOnHover = 'paused';
+
+// Styling property
+const containerBackgroundColor = 'black';
+const itemTextColor = 'yellow';
+const itemFontSize = '1.3rem';
+const itemFontWeight = 500;
+const itemLetterSpacing = '1px';
+const itemLineHeight = '1.6em';
+const itemPaddingTop = '10px';
+const itemPaddingBottom = '10px';
+
+// Message styling data
+const messageContainerBackgroundColor = 'white';
+const messageContainerPosition = 'left';
+const messageTextColor = 'black';
+const messageFontSize = '1.5rem';
+const messageFontWeight = 900;
+const messageLetterSpacing = '1px';
+const messageLineHeight = '1.6em';
+const messagePaddingTop = '10px';
+const messagePaddingBottom = '10px';
+const messagePaddingLeft = '10px';
+const messagePaddingRight = '10px';
+
+
+// Styling
+setPropertyOnDocument( '--oqtave-custom-ticker-container-background-color', containerBackgroundColor );
+// Item text color
+setPropertyOnDocument( '--oqtave-custom-ticker-item-text-color', itemTextColor );
+// Item font size
+setPropertyOnDocument( '--oqtave-custom-ticker-item-font-size', itemFontSize );
+// Item font weight
+setPropertyOnDocument( '--oqtave-custom-ticker-item-font-weight', itemFontWeight );
+// Item letter spacing
+setPropertyOnDocument( '--oqtave-custom-ticker-item-letter-spacing', itemLetterSpacing );
+// Item line height
+setPropertyOnDocument( '--oqtave-custom-ticker-item-line-height', itemLineHeight );
+// Item padding top
+setPropertyOnDocument( '--oqtave-custom-ticker-item-padding-top', itemPaddingTop );
+// Item padding bottom
+setPropertyOnDocument( '--oqtave-custom-ticker-item-padding-bottom', itemPaddingBottom );
+
+// Styling message
+// Position
+setPropertyOnDocument( '--oqtave-custom-ticker-message-section-position-left', messageContainerPosition == 'left' ? 0 : 'auto' );
+setPropertyOnDocument( '--oqtave-custom-ticker-message-section-position-right', messageContainerPosition == 'right' ? 0 : 'auto' );
+// Background color
+setPropertyOnDocument( '--oqtave-custom-ticker-message-section-background-color', messageContainerBackgroundColor );
+// Text
+setPropertyOnDocument( '--oqtave-custom-ticker-message-text-color', messageTextColor );
+setPropertyOnDocument( '--oqtave-custom-ticker-message-font-size', messageFontSize );
+setPropertyOnDocument( '--oqtave-custom-ticker-message-font-weight', messageFontWeight );
+
+
+
 // Get ticker wrapper
 const oqtaveCustomTickerWrapper = document.querySelector('.oqtave-custom-ticker-wrapper');
 
 // Generate seconds
-console.log( generateAnimationSeconds( oqtaveCustomTickerWrapper.scrollWidth, 1 ) );
+const animationSeconds = generateAnimationSeconds( oqtaveCustomTickerWrapper.scrollWidth, tickerSpeed );
+
+// Set ticker animation time 
+setPropertyOnDocument( '--oqtave-custom-ticker-animation-time', `${animationSeconds}s`);
+// Set ticker position
+setPropertyOnDocument( '--oqtave-custom-ticker-move-left-end', `-${oqtaveCustomTickerWrapper.scrollWidth}px`);
+setPropertyOnDocument( '--oqtave-custom-ticker-move-right-start', `-${oqtaveCustomTickerWrapper.scrollWidth}px`);
+// Set ticker animation name
+setPropertyOnDocument( '--oqtave-custom-ticker-animation-name', `oqtaveCustomTickerMove${tickerDirection.charAt(0).toUpperCase() + tickerDirection.slice(1)}`);
+// Set ticker animation delay
+setPropertyOnDocument( '--oqtave-custom-ticker-animation-delay', `${tickerDelay}s`);
+// Set ticker start position
+setPropertyOnDocument( '--oqtave-custom-ticker-start', `var(--oqtave-custom-ticker-move-${tickerDirection == 'right' ? 'right-start': 'left-start'})`);
+// Set ticker on hover
+setPropertyOnDocument( '--oqtave-custom-ticker-animation-on-hover', tickerOnHover );
+
+// Play ticker animation
+setPropertyOnDocument( '--oqtave-custom-ticker-animation-state', `playing`);
+
+
+// ! Fetch data asc on right animation
